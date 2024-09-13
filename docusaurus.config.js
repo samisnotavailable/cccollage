cf5b935e5368a33fa30b16b94ad98e54c75fbf5b
Samuel Noel <samuelnoel67@gmail.com>
2024-09-13 23:40:27 +0800

    github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,15 +14,16 @@ const config = {
   favicon: 'img/cccollage.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/cccollage/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'samisnotavailable',
+  projectName: 'cccollage', 
+  
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
