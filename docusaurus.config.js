53129f1133ee911c7b26f31ea67014dd2093679f
Samuel Noel <samuelnoel67@gmail.com>
2024-09-14 02:07:54 +0800

    forgor

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   favicon: 'img/cccollage.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/samisnotavailable',
+  url: 'https://samisnotavailable.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/cccollage/',
